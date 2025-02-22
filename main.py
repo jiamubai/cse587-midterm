ac31daca8e8f612739ba0ed62070e16f80fc46d0
jiamubai <jb7082@nyu.edu>
2025-02-22 15:33:46 -0500

    fix_bug

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+os.environ['TORCH_USE_CUDA_DSA'] = '1'
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
-# from torchtext.datasets import IMDB, YelpReviewPolarity
-# from transformers import BertTokenizer, BertModel
-from sklearn.metrics import classification_report, accuracy_score
-from tqdm import tqdm
-from utils import load_dataset, train_model, evaluate_model, CNNModel, RNNModel
-
-# For Twitter Sentiment140, you might need to download and preprocess separately
-# Assuming we have preprocessed all datasets into a standard CSV format with 'text' and 'label' columns
-import pandas as pd
-# from torchtext.data.utils import get_tokenizer
+from torch.utils.data import DataLoader
+from utils import load_dataset, train_model, CNNModel, RNNModel
 import gensim.downloader as api
 import argparse
+import logging
 
-# Main execution (example usage)
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='rnn', help='Model to use: cnn, rnn')
     parser.add_argument('--dataset', type=str, default='yelp', help='Dataset to use: imdb, yelp, twitter')
-    parser.add_argument('--batch_size', type=int, default=32, help='Training batch size')
+    parser.add_argument('--batch_size', type=int, default=1024, help='Training batch size')
     parser.add_argument('--max_len', type=int, default=128, help='Maximum sequence length')
     parser.add_argument('--epoch', type=int, default=10, help='Training epoch')
     parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
+    parser.add_argument('--seed', type=int, default=42, help='Random seed')
     args = parser.parse_args()
     print(args)
+    torch.manual_seed(args.seed)
+
+    logging.basicConfig(filename=f'result_{args.model}_{args.dataset}_epoch_{args.epoch}_lr_{args.lr}.log',
+                        # format='%(asctime)s - %(levelname)s - %(message)s',
+                        level=logging.INFO,
+                        filemode='w')
 
 
     # Load GloVe embeddings
@@ -35,10 +35,13 @@ def main():
     device = 'cuda:0'
     if args.dataset == 'imdb':
         data_path = 'dataset/processed_imdb_data.csv'
+        num_class = 2
     elif args.dataset == 'yelp':
         data_path = 'dataset/processed_yelp_data.csv'
+        num_class = 5
     elif args.dataset == 'twitter':
         data_path = 'dataset/twitter_raw.csv'
+        num_class = 3
     else:
         raise ValueError('Invalid dataset')
     train_dataset, test_dataset = load_dataset(data_path, embedding_model)
@@ -46,9 +49,9 @@ def main():
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
 
     if args.model == 'rnn':
-        model = RNNModel(embedding_dim=50, hidden_dim=128, num_classes=2, n_layers=1).to(device)
+        model = RNNModel(embedding_dim=50, hidden_dim=128, num_classes=num_class, n_layers=1).to(device)
     else:
-        model = CNNModel(embedding_dim=50, num_classes=2, max_len=args.max_len).to(device)
+        model = CNNModel(embedding_dim=50, num_classes=num_class, max_len=args.max_len).to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     criterion = nn.CrossEntropyLoss()
 
--- utils/func.py
+++ utils/func.py
@@ -4,25 +4,29 @@ from tqdm import tqdm
 import torch
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
+import logging
 
 # Load and preprocess datasets
 def load_dataset(file_path, embedding_model, max_len=128, test_size=0.2):
     if 'imdb' in file_path:
         df = pd.read_csv(file_path)
         df = df[(df['sentiment_id'] <= 3)|(df['sentiment_id'] >=8)]
+        texts = df['review'].tolist()
+        labels = df['sentiment'].tolist()
+        labels = [0 if label == 'negative' else 1 for label in labels]
     elif 'yelp' in file_path:
         df = pd.read_csv(file_path)
-        df = df[df['stars'] != 3]
+        texts = df['review'].tolist()
+        labels = df['stars'].tolist()
+        labels = [label - 1 for label in labels]
+        # df = df[df['stars'] != 3]
     else:
         df = pd.read_csv(file_path, encoding="ISO-8859-1", engine="python", on_bad_lines='skip')
         df.columns = ["sentiment", "time", "date", "query", "username", "review"]
-        df = df[df['sentiment'] != 2]
-    texts = df['review'].tolist()
-    labels = df['sentiment'].tolist()
-    if 'twitter' in file_path:
-        labels = [0 if label == 0 else 1 for label in labels]
-    else:
-        labels = [0 if label == 'negative' else 1 for label in labels]
+        texts = df['review'].tolist()
+        labels = df['sentiment'].tolist()
+        labels = [label // 2 for label in labels]
+
     train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels, test_size=test_size, random_state=42)
     train_dataset = SentimentDataset(train_texts, train_labels, embedding_model, max_len)
     test_dataset = SentimentDataset(test_texts, test_labels, embedding_model, max_len)
@@ -32,6 +36,7 @@ def load_dataset(file_path, embedding_model, max_len=128, test_size=0.2):
 def train_model(model, train_loader, test_loader, criterion, optimizer, device, epochs=10):
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1}/{epochs}")
+        logging.info(f"Epoch {epoch + 1}/{epochs}")
         model.train()
         total_loss = 0
         for batch in tqdm(train_loader, desc='Training'):
@@ -45,16 +50,25 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, device, 
             optimizer.step()
             total_loss += loss.item()
         print(f'Training Loss: {total_loss / len(train_loader)}')
+        logging.info(f'Training Loss: {total_loss / len(train_loader)}')
 
         print('Evaluating on Training Set...')
+        logging.info('Evaluating on Training Set...')
+
         train_report, train_accuracy = evaluate_model(model, train_loader, device)
         print(train_report)
+        logging.info(train_report)
         print(f'Training Accuracy: {train_accuracy}')
+        logging.info(train_report)
 
         print('Evaluating on Test Set...')
+        logging.info('Evaluating on Test Set...')
+
         test_report, test_accuracy = evaluate_model(model, test_loader, device)
         print(test_report)
+        logging.info(test_report)
         print(f'Test Accuracy: {test_accuracy}')
+        logging.info(test_report)
 
 @torch.no_grad()
 def evaluate_model(model, dataloader, device):
